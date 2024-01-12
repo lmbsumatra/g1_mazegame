dffe02cc4cf00f0dff06f5f406386725c03014cf
NissinNudels <123383430+NissinNudels@users.noreply.github.com>
2024-01-12 21:23:10 +0800

    door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Sprite openDoorSprite;
+    public Sprite closedDoorSprite;
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+    private bool isDoorOpen = false;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        if (spriteRenderer == null || boxCollider == null)
+        {
+            Debug.LogError("SpriteRenderer or BoxCollider2D not found on the GameObject with Door script.");
+        }
+
+        CloseDoor();
+    }
+
+    void Update()
+    {
+        if (KeyScoreManager.keyAmount >= 3 && !isDoorOpen)
+        {
+            OpenDoor();
+        }
+        else if (KeyScoreManager.keyAmount < 3 && isDoorOpen)
+        {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        spriteRenderer.sprite = openDoorSprite;
+        boxCollider.enabled = false;
+        isDoorOpen = true;
+    }
+
+    private void CloseDoor()
+    {
+        spriteRenderer.sprite = closedDoorSprite;
+        boxCollider.enabled = true;
+        isDoorOpen = false;
+    }
+}
