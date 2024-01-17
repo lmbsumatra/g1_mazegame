c6770d19d600a4fa2bcc397e547fac718d2821ad
NissinNudels <123383430+NissinNudels@users.noreply.github.com>
2024-01-17 17:16:54 +0800

    FIXED UI

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float zoomFactor = 3.0f; // Increase this value for a closer view
+    private Vector3 velocity = Vector3.zero;
+
+    private Camera mainCamera;
+
     void Start()
     {
-        
+        mainCamera = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
     {
-        
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        // Adjust camera's orthographic size for zooming
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, 5f / zoomFactor, Time.deltaTime);
     }
 }
